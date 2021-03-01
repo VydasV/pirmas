0d241d1f63b7e516eb0e49eb88cb6c03fb76e9d3
VydasV <v.vydas@gmail.com>
2021-03-01 19:53:03 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+from services import pirmas
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Event(db.Model):
+    # __tablename__ = 'events' - jei db bus daugiau lentelių
+    # apibrėžiam duombazės stulpelius
+    id = db.Column(db.Integer, primary_key=True)
+    event_text = db.Column(db.String(300), nullable=False)
+    event_date = db.Column(db.Date)
+
+    def __repr__(self):
+        return '<Event %r>' % self.id
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    """atvaizduoja pradžios puslapį"""
+    return render_template('index.html', title='"Kardo ir žagrės" sąjunga')
+
+
+@app.route('/about')
+def about():
+    """atvaizduoja apie mus puslapį"""
+    apie = pirmas.add_aboutus_text_from_file()
+    return render_template('about.html', title='Apie mus', apie=apie)
+
+
+@app.route('/posts')
+def posts():
+    """atvaizduoja įrašus apie įvykius"""
+    events = Event.query.order_by(Event.event_date.desc()).all()
+    return render_template('posts.html', title='Pranešimai', events=events)
+
+
+@app.route('/post/<int:id>')
+def post_view(id):
+    """atvaizduoja pasirinktą įrašą su konkrečiu id"""
+    event = Event.query.get(id)
+    return render_template('post_view.html', title='Pranešimas', event=event)
+
+
+@app.route('/post/<int:id>/delete')
+def post_delete(id):
+    """pašalina iš bazės pasirinktą įrašą su konkrečiu id"""
+    event = Event.query.get_or_404(id)
+
+    try:
+        db.session.delete(event)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return 'Įvyko klaida trinant įrašą iš "Kardo ir žagrės" sąjungos!'
+
+
+@app.route('/post/create', methods=['POST', 'GET'])
+def create_event():
+    """sukuria naują duomenų bazės įrašą"""
+    if request.method == "POST":
+        # date grąžina str, todėl reikia konvertuoti į datos tipą
+        event_date = date.fromisoformat(request.form['date'])
+        event_text = request.form['text']
+
+        record = Event(event_text=event_text, event_date=event_date)
+
+        try:
+            db.session.add(record)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'Įvyko klaida įrašant pranešimą apie "Kardo ir žagrės" sąjungą!'
+    else:
+        return render_template('post_create.html', title='Naujas pranešimas')
+
+
+@app.route('/post/<int:id>/modify', methods=['POST', 'GET'])
+def modify_event(id):
+    """Koreguoja pagal id iš bazės pasirinktą įrašą"""
+    event = Event.query.get(id)
+    if request.method == "POST":
+        # date grąžina str, todėl reikia konvertuoti į datos tipą
+        event.event_date = date.fromisoformat(request.form['date'])
+        event.event_text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'Įvyko klaida redaguojant pranešimą apie "Kardo ir žagrės" sąjungą!'
+    else:
+        return render_template('post_modify.html', title='Redaguoti įvykį', event=event)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ services/pirmas.py
@@ -0,0 +1,16 @@
+def add_aboutus_text_from_file() -> str:
+    """grąžina iš failo apie_mus.txt tekstą puslapiui apie,
+       arba string tipo kintamąjį text, jei tokio failo nėra
+       """
+    
+    try:
+        with open('.\\static\\apie_mus.txt', 'r', encoding='utf-8') as file:
+            text = ' '.join(file.readlines())
+    except FileNotFoundError:
+        text = 'slapta "Kardo ir žagrės" sąjunga - mūsų rankos ilgos!!!'
+    return text
+
+
+if __name__ == "__main__":
+    a = add_aboutus_text_from_file()
+    print(a)
